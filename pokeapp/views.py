fe7423e0da980ea5752fcb6905ccff3ba279ef9a
KaumudiSoman <kaumudi814@gmail.com>
2023-09-16 17:38:40 +0530

    first commit

--- pokeapp/views.py
+++ pokeapp/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 import requests
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 
 def hello(request):
     return render(request, 'index.html')
-    #return HttpResponse('Hello! Welcome to Pokedex')
 
 def pokemon_types(request):
     # API endpoint for Pokémon types
@@ -21,7 +20,6 @@ def pokemon_types(request):
         types = [type_info['name'] for type_info in data['results']]
         dtypes = {'pokemon_types': types}
         
-        #return JsonResponse({'pokemon_types': types})
         return render(request, 'types.html', dtypes)
     except Exception as e:
         return JsonResponse({'error': str(e)})
@@ -40,7 +38,6 @@ def pokemon_versions(request):
         version = [version_info['name'] for version_info in data['results']]
         dversion = {'pokemon_versions': version}
         
-        #return JsonResponse({'pokemon_versions': dversion})
         return render(request, 'versions.html', dversion)
     except Exception as e:
         return JsonResponse({'error': str(e)})
@@ -59,7 +56,6 @@ def pokemon_ability(request):
         ability = [ability_info['name'] for ability_info in data['results']]
         dability = {'pokemon_ability': ability}
         
-        #return JsonResponse({'pokemon_ability': dability})
         return render(request, 'ability.html', dability)
     except Exception as e:
         return JsonResponse({'error': str(e)})
